b8df8b0f7b7e2afc782ffcac09fc99e342b0271d
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2022-03-23 23:41:43 +0530

    feat: randomized indices

--- cpp/src/arrow/compute/exec/scheduler_benchmark.cc
+++ cpp/src/arrow/compute/exec/scheduler_benchmark.cc
@@ -19,27 +19,27 @@ namespace arrow {
 namespace internal {
 
 struct Workload {
-  explicit Workload(int32_t size) : size_(size), data_(kDataSize) {
+  explicit Workload(int32_t size) : data_(size), size_(size), indices_(size) {
     std::default_random_engine gen(42);
     std::uniform_int_distribution<uint64_t> dist(0, std::numeric_limits<uint64_t>::max());
     std::generate(data_.begin(), data_.end(), [&]() { return dist(gen); });
+    
+    std::iota(indices_.begin(), indices_.end(), 0);
+    std::shuffle(indices_.begin(), indices_.end(), std::default_random_engine(42));  
   }
 
   void operator()();
 
  private:
-  static constexpr int32_t kDataSize = 32;
-
-  int32_t size_;
   std::vector<uint64_t> data_;
+  uint64_t size_;
+  std::vector<uint64_t> indices_;
 };
 
 void Workload::operator()() {
   uint64_t result = 0;
-  for (int32_t i = 0; i < size_ / kDataSize; ++i) {
-    for (const auto v : data_) {
-      result = (result << (v % 64)) - v;
-    }
+  for (uint64_t i = 0; i<size_; ++i) {
+    result = (result << (data_[indices_[i]] % 64)) - data_[indices_[i]];
   }
   benchmark::DoNotOptimize(result);
 }
@@ -57,7 +57,7 @@ struct Task {
 };
 
 static void GroupedExecution(benchmark::State& state) {  // NOLINT non-const reference
-  const auto workload_size = static_cast<int32_t>(state.range(0));
+  const auto workload_size = static_cast<int64_t>(state.range(0));
 
   Workload workload(workload_size);
 
@@ -68,19 +68,19 @@ static void GroupedExecution(benchmark::State& state) {  // NOLINT non-const ref
 }
 
 static void BatchedExecution(benchmark::State& state) {  // NOLINT non-const reference
-  const auto workload_size = static_cast<int32_t>(state.range(0));
-  const auto batch_size = static_cast<int32_t>(state.range(1));
+  const auto workload_size = static_cast<int64_t>(state.range(0));
+  const auto batch_size = 8000;
   Workload workload(batch_size);
 
   for (auto _ : state) {
-    for(int i = 0; i < workload_size; i+= batch_size){
+    for (int i = 0; i < workload_size; i += batch_size) {
       workload();
       workload();
     }
   }
 }
 
-BENCHMARK(GroupedExecution)->Arg(1000000);
-BENCHMARK(BatchedExecution)->Args({1000000,8000});
-}
-}
+BENCHMARK(GroupedExecution)->RangeMultiplier(2)->Range(2<<10, 2<<29);
+BENCHMARK(BatchedExecution)->RangeMultiplier(2)->Range(2<<10, 2<<29);
+}  // namespace internal
+}  // namespace arrow
